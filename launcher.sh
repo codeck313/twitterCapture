4f89612c46304ea343d9533bee2a906ca3e230ff
Codeck <23121752+codeck313@users.noreply.github.com>
2019-03-13 00:52:49 +0530

    Added placeholder for cd

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,7 @@
 #launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
 
-cd /home/pi/indCap
+cd /path/to/projectDIR
 
 if [ ! $(pgrep -f tweetCapture.py) ]; then
 	    echo "Not Running So starting"
